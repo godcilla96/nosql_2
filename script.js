7ad7adba29bbc0ca534a3a49d2c2d2c4456b2561
Cecilia Lindström <cecilia.a.lindstrom@live.se>
2024-04-30 21:05:46 +0200

    Skriver ut data till sidan

--- script.js
+++ script.js
@@ -1,2 +1,45 @@
-const url = "mongodb://127.0.0.1:27017/cv";
+const serverUrl = 'http://localhost:3003';
 
+// hämta in workexperience 
+/*
+async function fetchWorkExperiences() {
+    try {
+        const response = await fetch(`${serverUrl}/workexperience`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch work experiences');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching work experiences:', error);
+        return [];
+    }
+}
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch(`${serverUrl}/workexperience`, {
+      method: 'GET'
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        // Presentera datan i en lista
+        const postList = document.getElementById('postList');
+        data.forEach(post => {
+          const listItem = document.createElement('li');
+          listItem.textContent = `${post.companyname} - ${post.jobtitle}, ${post.startdate}, ${post.enddate}: ${post.description}`;
+
+          
+          postList.appendChild(listItem);
+        });
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  });
